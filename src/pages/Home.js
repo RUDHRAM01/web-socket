dbbbd1908f893ed0df0f6b1c2e64e6fdc3f6c78d
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-07-28 14:26:38 +0530

    signin ans signup page added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Landing from '../components/landing/Landing';
 import Chat from '../components/chat/Chat';
+import SignIn from "../components/login/SignIn"; 
+import SignUp from "../components/login/SignUp";
 function Home() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
       </Routes>
     </div>
   );
