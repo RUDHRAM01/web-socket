6f4d501462563943db8128776e089b9bfe73ffb1
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-10 15:11:45 +0530

    adding chat

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ChatArea from './chatarea/ChatArea'
 import AllChat from './allchat/AllChat'
+import { Hidden } from '@mui/material'
+import '../styles.css'
 
 function Chat() {
     return (
         <div>
-            <div className='chatMain' style={{ display: "flex", justifyContent: "space-between", padding: "20px" }}>
-                <AllChat />
+            <div className='chatMain' style={{ display: "flex", justifyContent: "space-between", padding: "20px",backgroundColor:"black",height:"100vh" }}>
+                <Hidden mdDown><AllChat /></Hidden>
                 <ChatArea />
             </div>
         </div>
