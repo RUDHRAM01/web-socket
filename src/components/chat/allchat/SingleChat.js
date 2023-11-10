5c821a355b58b1c2cab21b1be0d16bee991d84a0
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-10 15:12:17 +0530

    adding singlechat

--- src/components/chat/allchat/SingleChat.js
+++ src/components/chat/allchat/SingleChat.js
@@ -2,13 +2,17 @@ import React from 'react'
 import Typography from '@mui/material/Typography'
 import { Avatar } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import { setOpen } from '../../../reducer/UiSlice'
+import { useDispatch } from 'react-redux'
+
 
 
 function SingleChat({ props }) {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
     
     return (
-        <button style={{ width: "100%" }} onClick={() => navigate(`/${props?.id}`)} key={`${props?.id}`}>
+        <button style={{ width: "100%" }} onClick={() => { navigate(`/${props?.id}`); dispatch(setOpen(false))}} key={`${props?.id}`}>
             <div style={{ display: "flex", padding: "10px", alignItems: "center", gap: "8px", backgroundColor: "Gray", margin: "2px", borderRadius: "4px" }}>
                 <Avatar src={props?.imageUrl} alt='img' />
                 <div>
