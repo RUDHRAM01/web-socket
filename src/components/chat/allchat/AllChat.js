2269eaf12d94b171308d1e9ba162040e8530e8cf
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-10 15:12:04 +0530

    adding allchat

--- src/components/chat/allchat/AllChat.js
+++ src/components/chat/allchat/AllChat.js
@@ -6,8 +6,9 @@ import SearchUser from '../../navigation/SearchUser'
 
 function AllChat() {
     return (
-        <div style={{ width: "30%", height: "94vh" }}>
-            <Title title={`Let's chat`} size={'60px'} />
+        <div style={{ height: "94vh",backgroundColor:"black",borderRadius:"4px",padding:"8px", border:"1px solid gray" }} className='AllChat'>
+            <Title title={`Let's chat`} size={'60px'} color={"white"} />
+            <hr style={{color:"white"}}/>
             <Navigation />
             <MyChat />
             <SearchUser />
