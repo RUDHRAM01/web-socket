a79d9bb674ce48d4f52bde34547eca6123218a33
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-13 12:52:40 +0530

    home page update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Chat from '../components/chat/Chat';
 import ChatLanding from '../components/chat/ChatLanding';
+import { useSelector } from "react-redux";
+import Login from './Login';
+import CreateAccount from './CreateAccount';
+
+
 
 
 function Home() {
+  const navigate = useNavigate( )
+  const login = useSelector((state) => state.userStore.isLogin);
+  console.log(login);
+  
+  useEffect(() => {
+
+    if (!login && window.location.pathname === "/create-account") {
+      navigate("/create-account")
+    } else if(!login){
+      navigate("/login")
+    }
+  }, [login,navigate])
+
   return (
     <div>
       <Routes>
-      <Route path="/" element={<ChatLanding />} />
-        <Route path="/:id" element={<Chat />} />
+        {
+          login ? <>
+            <Route path="/" element={<ChatLanding />} />
+            <Route path="/:id" element={<Chat />} />
+          </> : <>
+            <Route path='/login' element={<Login />} />
+            <Route path='/create-account' element={<CreateAccount />} />
+          </>}
       </Routes>
     </div>
   );
