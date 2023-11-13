8d7b75a6ed9d3d170c5e98e73e8c21860034c00d
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-13 12:51:54 +0530

    store update

--- src/store.js
+++ src/store.js
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit"
 import Slice from "./reducer/Slice"
 import UiSlice from "./reducer/UiSlice"
+import userSlice from "./reducer/userSlice"
+
+
 
 
 const store = configureStore({
     reducer: {
         'chatStore': Slice,
-        'uiStore': UiSlice
+        'uiStore': UiSlice,
+        'userStore': userSlice,
     }
 })
 
