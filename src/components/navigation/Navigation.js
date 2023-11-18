bf66646a038852cca38debcabd27cb1a6bca305c
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-18 13:16:51 +0530

    navigation update

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,7 +5,7 @@ import { setOpen } from '../../reducer/UiSlice'
 import { useDispatch } from 'react-redux'
 
 
-function Navigation() {
+function Navigation({handleOpen}) {
   const dispatch = useDispatch()
   
   return (
@@ -15,7 +15,7 @@ function Navigation() {
           <AiOutlineSearch />
           <Typography variant="overline" style={{ color: "white" }}>Search...</Typography>
         </Button>
-        <Avatar src='https://th.bing.com/th?id=OIP.jG20xZ4rIJfvN9q5oN2OBwHaEo&w=316&h=197&c=8&rs=1&qlt=30&o=6&dpr=1.3&pid=3.1&rm=2' alt='user' />
+        <Avatar style={{cursor:"pointer"}} src='https://th.bing.com/th?id=OIP.jG20xZ4rIJfvN9q5oN2OBwHaEo&w=316&h=197&c=8&rs=1&qlt=30&o=6&dpr=1.3&pid=3.1&rm=2' alt='user' onClick={handleOpen} />
       </div>
     </>
   )
