d50e1f6bcfd3dfabb107e6fed666d0585bfef982
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-18 13:16:31 +0530

    all chat update

--- src/components/chat/allchat/AllChat.js
+++ src/components/chat/allchat/AllChat.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navigation from '../../navigation/Navigation'
 import Title from '../../common/Title'
 import MyChat from './MyChat'
 import SearchUser from '../../navigation/SearchUser'
+import Profile from '../../navigation/Profile'
 
 function AllChat() {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    
+    
     return (
         <div style={{ height: "94vh",backgroundColor:"black",borderRadius:"4px",padding:"8px", border:"1px solid gray" }} className='AllChat'>
             <Title title={`Let's chat`} size={'60px'} color={"white"} />
+            <Profile open={open} setOpen={setOpen}/>
             <hr style={{color:"white"}}/>
-            <Navigation />
+            <Navigation handleOpen={handleOpen} />
             <MyChat />
             <SearchUser />
         </div>
