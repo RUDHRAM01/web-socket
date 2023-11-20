7c48de5c5aa07f8ee4d590d94ba357ac140ac16e
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-20 21:27:58 +0530

    allchat update

--- src/components/chat/allchat/AllChat.js
+++ src/components/chat/allchat/AllChat.js
@@ -1,34 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, {  useState } from 'react'
 import Navigation from '../../navigation/Navigation'
 import Title from '../../common/Title'
 import MyChat from './MyChat'
 import SearchUser from '../../navigation/SearchUser'
 import Profile from '../../navigation/Profile'
-import { getAllChats } from '../../../api/get/getAllChats'
-import { useDispatch, useSelector } from 'react-redux'
-import { setChatData } from '../../../reducer/Slice'
 
 
-function AllChat() {
-  const storeChatData = useSelector((state) => state.chatStore.data);
-  const [chatData, setChat] = useState(storeChatData)
-  const [open, setOpen] = useState(false);
-  const dispatch = useDispatch();
-  const handleOpen = () => setOpen(true);
-  let path = window.location.pathname 
-  console.log("path : ",path);
+
+
+function AllChat({chatData}) {
   
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
 
-  useEffect(() => {
-    const calling = async () => {
-      const { data } = await getAllChats()
-      console.log(data);
-      
-      dispatch(setChatData(data));
-      setChat(data)
-    }
-    calling()
-  }, [dispatch])
+
 
   return (
     <div style={{ height: "94vh", backgroundColor: "black", borderRadius: "4px", padding: "8px", border: "1px solid gray" }} className='AllChat'>
