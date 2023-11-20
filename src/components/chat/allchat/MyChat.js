af151de05543411847a47b6bd1d76e425a33775c
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-20 21:28:07 +0530

    my chat update

--- src/components/chat/allchat/MyChat.js
+++ src/components/chat/allchat/MyChat.js
@@ -3,7 +3,8 @@ import SingleChat from './SingleChat'
 import Typography from '@mui/material/Typography'
 // import data from "../../../common-data/Common.json"
 
-function MyChat({data}) {
+function MyChat({ data }) {
+  
   return (
       <div>
           {data.length > 0 ? data.map((item) => <SingleChat props={item} key={item?._id}/>) : <Typography variant="subtitle2" style={{color:"white"}}>No Chat</Typography> }
