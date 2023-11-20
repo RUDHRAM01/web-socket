0bd5eddec5e4d7ab02c142a928398e76664fed0a
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-20 21:28:16 +0530

    single chat update

--- src/components/chat/allchat/SingleChat.js
+++ src/components/chat/allchat/SingleChat.js
@@ -5,23 +5,23 @@ import { useNavigate } from 'react-router-dom'
 import { setOpen } from '../../../reducer/UiSlice'
 import { useDispatch } from 'react-redux'
 
-
-
-
 function SingleChat({ props }) {
+    var data = localStorage.getItem('loginInfo');
+    data = JSON.parse(data);
     const navigate = useNavigate();
     const dispatch = useDispatch();
-
+    const value = props?.users[0]?._id === data?.id ? props?.users[1] : props?.users[0]
+    
     return (
-        <button style={{ width: "100%" }} onClick={() => { navigate(`/chat/${props?.users[0]?._id}`); dispatch(setOpen(false))}} key={`${props?._id}`}>
+        <button style={{ width: "100%" }} onClick={() => { navigate(`/chat/${value?._id}`); dispatch(setOpen(false))}} key={`${value?._id}`}>
             <div style={{ display: "flex", padding: "10px", alignItems: "center", gap: "8px", backgroundColor: "Gray", margin: "2px", borderRadius: "4px" }}>
-                <Avatar src={props?.users[0]?.imageUrl} alt='img' />
+                <Avatar src={value?.profilePic} alt='img' />
                 <div>
                     <Typography variant="body1" style={{ color: "pink" }}>
-                        {props?.users[0]?.name}
+                        {value?.name}
                     </Typography>
                     <Typography variant="body2" style={{ color: "white" }}>
-                        {props?.users[0]?.latestMessage?.content}
+                        {value?.latestMessage?.content}
                     </Typography>
                 </div>
             </div>
