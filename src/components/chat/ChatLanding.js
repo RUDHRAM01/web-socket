5dfa3226d92686f7a5ad32357957042cf5aec731
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-20 21:27:52 +0530

    chat landing update

--- src/components/chat/ChatLanding.js
+++ src/components/chat/ChatLanding.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AllChat from './allchat/AllChat'
 import NoChat from './chatarea/Nochat'
 import { Hidden } from '@mui/material'
-
+import { getAllChats } from '../../api/get/getAllChats'
+import { useDispatch } from 'react-redux'
 
 function ChatLanding() {
+    const dispatch = useDispatch();
+    const [chatData, setChat] = useState([])
+    useEffect(() => {
+        const calling = async () => {
+            const { data } = await getAllChats()
+            setChat(data)
+        }
+        calling()
+    }, [dispatch])
+
+
     return (
         <div>
             <div className='chatMain' style={{ display: "flex", justifyContent: "space-between", padding: "20px" }}>
-                <AllChat />
+                <AllChat chatData={chatData} />
                 <Hidden mdDown> <NoChat /></Hidden>
             </div>
         </div>
