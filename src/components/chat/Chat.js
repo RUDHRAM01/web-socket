d2e1838059073b0912f24fcf4a0a65809bd5d74e
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-20 21:27:41 +0530

    update

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,15 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ChatArea from './chatarea/ChatArea'
 import AllChat from './allchat/AllChat'
 import { Hidden } from '@mui/material'
 import '../styles.css'
+import { useParams } from 'react-router-dom'
+import io from 'socket.io-client';
+import toast from 'react-hot-toast';
+import { createChat } from '../../api/post/createChat';
+import { getAllChats } from '../../api/get/getAllChats'
 
+const ENDPOINT = 'http://localhost:4000';
+var socket;
 function Chat() {
+    const { id } = useParams();
+    // const [socketConnected, setSocketConnected] = useState(false);
+    const [chatUserInfo, setChatUserInfo] = useState({
+        isLoading: true
+    })
+    const [chatData, setChat] = useState([])
+
+    useEffect(() => {
+        if (id) {
+            socket = io(ENDPOINT);
+            socket.emit("setup", id);
+            socket.on("connection", () => {
+                console.log("connected");
+            });
+        }
+
+        const createChatFun = async () => {
+            if (id === "login") return;
+            try {
+                let { data } = await createChat({ userId: id });
+                setChatUserInfo(() => ({ isLoading: false, ...data }))
+                data  = await getAllChats()
+                setChat(data?.data)
+            } catch (error) {
+                toast.error(error?.response?.data?.msg)
+            }
+        };
+        createChatFun();
+    }, [id]);
+
+
+    console.log(chatUserInfo,"chatuserinfo")
     return (
         <div>
-            <div className='chatMain' style={{ display: "flex", justifyContent: "space-between", padding: "20px",backgroundColor:"black",height:"100vh" }}>
-                <Hidden mdDown><AllChat /></Hidden>
-                <ChatArea />
+            <div className='chatMain' style={{ display: "flex", justifyContent: "space-between", padding: "20px", backgroundColor: "black", height: "100vh" }}>
+                <Hidden mdDown><AllChat chatData={chatData} /></Hidden>
+                <ChatArea chatUserInfo={chatUserInfo} />
             </div>
         </div>
     )
