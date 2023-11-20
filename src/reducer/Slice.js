a85896b552f33b1a9c200c8ccd8d98bdba333ee9
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-20 15:29:44 +0530

    update

--- src/reducer/Slice.js
+++ src/reducer/Slice.js
@@ -4,14 +4,18 @@ const chatStore = createSlice({
     name: "chatStore",
     initialState: {
         isCreateGroup: false,
+        data : []
     },
     reducers: {
         setIsCreateGroup: (state, action) => {
             state.isCreateGroup = action.payload;
         },
+        setChatData: (state, action) => {
+            state.data = action.payload;
+        },
     },
 });
 
-export const { setIsCreateGroup } = chatStore.actions;
+export const { setIsCreateGroup,setChatData } = chatStore.actions;
 
 export default chatStore.reducer;
