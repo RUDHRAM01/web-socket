820e9019901f8194967c859b38f4f087db5fc6c0
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-22 14:12:32 +0530

    update

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -8,6 +8,8 @@ import io from 'socket.io-client';
 import toast from 'react-hot-toast';
 import { createChat } from '../../api/post/createChat';
 import { getAllChats } from '../../api/get/getAllChats'
+import { getMessageApi } from '../../api/get/getAllMessage'
+import { sendMessageApi } from '../../api/post/sendMessage'
 
 const ENDPOINT = 'http://localhost:4000';
 var socket;
@@ -42,14 +44,48 @@ function Chat() {
         };
         createChatFun();
     }, [id]);
+    const [chats, setChats] = useState([]);
+    const [label, setLabel] = useState("send a message...");
+    const [sending, setSending] = useState(false);
+    const [message, setMessage] = useState("");
+    var data = localStorage.getItem('loginInfo');
+    data = JSON.parse(data);
+    const sendMessage = async () => {
+        if (message.length === 0) return;
+        try {
+          setLabel("sending...")
+          setChats([...chats, { content: message, sender: { _id: data?.id } }])
+          const res = await sendMessageApi({ message: message, chatId: chatUserInfo?._id });
+          socket.emit("new message", res?.data?.newMessage);
+          setLabel("send a message...");
+          setSending(true);
+          setMessage("")
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    useEffect(() => {
+        socket.on("message received", async (newMessageReceived) => {
+            const { data } = await getMessageApi(chatUserInfo?._id);
+            setChats(data?.messages)
+        }
+    )})
+     
 
-
-    console.log(chatUserInfo,"chatuserinfo")
+    useEffect(() => {
+        if (chatUserInfo?.isLoading) return;
+        const calling = async () => {
+          const { data } = await getMessageApi(chatUserInfo?._id);
+          setChats(data?.messages)
+        }
+        calling();
+      }, [sending, chatUserInfo?.isLoading !== true])
+  
     return (
         <div>
             <div className='chatMain' >
                 <Hidden mdDown><AllChat chatData={chatData} /></Hidden>
-                <ChatArea chatUserInfo={chatUserInfo} />
+                <ChatArea chatUserInfo={chatUserInfo} chats={chats} message={message} sendMessage={sendMessage} setMessage={setMessage} label={label} socket={socket} />
             </div>
         </div>
     )
