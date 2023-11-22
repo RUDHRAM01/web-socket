1fe571904c9e952f7bd38d8fadbbe812d2692cbd
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-22 11:03:27 +0530

    nav update

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -3,21 +3,25 @@ import { Avatar, Button, Typography } from '@mui/material'
 import { AiOutlineSearch } from "react-icons/ai"
 import { setOpen } from '../../reducer/UiSlice'
 import { useDispatch } from 'react-redux'
+import Logo from '../../assests/logo.png'
 
 
-function Navigation({handleOpen}) {
+function Navigation({ handleOpen }) {
   const dispatch = useDispatch()
   var data = localStorage.getItem('loginInfo');
   data = JSON.parse(data);
-  
+
   return (
     <>
-      <div style={{ display: "flex", justifyContent: "space-between",padding:"8px" }}>
-        <Button onClick={()=>{dispatch(setOpen(true))}} style={{display:"flex",gap:"8px"}}>
-          <AiOutlineSearch />
-          <Typography variant="overline" style={{ color: "white" }}>Search...</Typography>
-        </Button>
-        <Avatar style={{cursor:"pointer"}} src={data?.profilePic} alt='user' onClick={handleOpen} />
+      <div style={{ display: "flex", flexDirection: "column", padding: "8px",height:"20%" }}>
+        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+          <img src={Logo} alt="logo" style={{ width: "100px" }} />
+          <Avatar style={{ cursor: "pointer", border:"2px solid white" }} src={data?.profilePic} alt='user' onClick={handleOpen} />
+        </div>
+        <button onClick={() => { dispatch(setOpen(true)) }} style={{ display: "flex", gap: "8px", backgroundColor: "white", alignItems: "center", height: "50px" }}>
+          <AiOutlineSearch style={{ marginLeft: "8px", }} />
+          <p style={{}}>Search...</p>
+        </button>
       </div>
     </>
   )
