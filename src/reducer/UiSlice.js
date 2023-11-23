3a69a6d7e51b9b21bd37a965d4ea6e75ff7065cf
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-23 19:52:38 +0530

    update

--- src/reducer/UiSlice.js
+++ src/reducer/UiSlice.js
@@ -4,14 +4,18 @@ const UiStore = createSlice({
     name: "UiStore",
     initialState: {
         open: false,
+        loading:false
     },
     reducers: {
         setOpen(state, action) {
             state.open = action.payload;
-        }
+        },
+        setLoading(state, action) {
+            state.loading = action.payload;
+        },
     },
 });
 
-export const { setOpen } = UiStore.actions;
+export const { setOpen,setLoading } = UiStore.actions;
 
 export default UiStore.reducer;
