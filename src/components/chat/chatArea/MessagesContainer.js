2ff9e3706b19fef3bfc2707c5eebcff8c0fb4480
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-23 19:51:49 +0530

    update

--- src/components/chat/chatArea/MessagesContainer.js
+++ src/components/chat/chatArea/MessagesContainer.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
+
 function MessagesContainer({ item, i, currentUser }) {
   let direction = currentUser === item?.sender?._id ? "flex-end" : "flex-start";
+  let className = currentUser === item?.sender?._id ? "end" : "start";
   return (
-      <>
-            <div style={{display:"flex",justifyContent:`${direction}`}}>
-                <div style={{backgroundColor:`${i%2===0?"lightgray":"lightblue"}`,padding:"8px",borderRadius:"8px",margin:"4px",maxWidth:"50%"}}>
-                    <p>{item?.content}</p>
-              </div>
-            </div>
-      </>
+    <>
+      <div style={{ display: "flex", justifyContent: `${direction}` }}>
+        <div className={className} style={{ padding: "8px", margin: "4px", maxWidth: "50%" }}>
+          <p>{item?.content}</p>
+        </div>
+      </div>
+      
+    </>
   )
 }
 
