07b50980416b2cc47f90003031bc46e41b22be95
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-23 19:51:36 +0530

    update

--- src/components/chat/allchat/SingleChat.js
+++ src/components/chat/allchat/SingleChat.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Typography from '@mui/material/Typography'
 import { Avatar } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
@@ -6,21 +6,23 @@ import { setOpen } from '../../../reducer/UiSlice'
 import { useDispatch } from 'react-redux'
 
 function SingleChat({ props }) {
-    var data = localStorage.getItem('loginInfo');
-    data = JSON.parse(data);
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const value = props?.users[0]?._id === data?.id ? props?.users[1] : props?.users[0]
-    
+    var data = localStorage.getItem('loginInfo');
+    data = JSON.parse(data);
+
+    const chatWithUser = props?.users?.find((user) => user?._id !== data?.id);
+
+
     return (
-        <button style={{ width: "100%" }} onClick={() => { navigate(`/chat/${value?._id}`); dispatch(setOpen(false))}} key={`${value?._id}`}>
-            <div style={{ display: "flex", padding: "10px", alignItems: "center", gap: "8px", backgroundColor:"GrayText", margin: "2px", borderRadius: "4px" }}>
-                <Avatar src={value?.profilePic} alt='img' />
-                <div style={{display:"flex",flexDirection:"column",width:"100%",alignItems:"flex-start"}}>
-                    <Typography variant="body1" style={{ color: "orange" }}>
-                        {value?.name}
+        <button style={{ width: "100%" }} onClick={() => { navigate(`/chat/${props?._id}`); dispatch(setOpen(false)) }} key={`${props?._id}`}>
+            <div style={{ display: "flex", padding: "10px", alignItems: "center", gap: "8px", margin: "2px" }} className='chatUserCard'>
+                <Avatar src={chatWithUser?.profilePic} alt='img' />
+                <div style={{ display: "flex", flexDirection: "column", width: "100%", alignItems: "flex-start" }}>
+                    <Typography variant="body1" style={{ color: "black", fontSize: "16px", fontWeight: "600",textOverflow: "ellipsis",maxWidth:"85%", whiteSpace: "nowrap",overflow: "hidden" }}>
+                        {chatWithUser?.name}
                     </Typography>
-                    <Typography variant="body2" style={{ color: "white" }}>
+                    <Typography variant="body2" style={{ color: "#3498db", overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap",maxWidth:"50%" }}>
                         {props?.latestMessage?.content}
                     </Typography>
                 </div>
