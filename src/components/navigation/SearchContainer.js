c78c45c748948aad107c8bfd569638170ae49ddc
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-23 19:52:06 +0530

    update

--- src/components/navigation/SearchContainer.js
+++ src/components/navigation/SearchContainer.js
@@ -3,23 +3,35 @@ import { Avatar, Typography } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { setOpen } from '../../reducer/UiSlice'
+import { createChat } from '../../api/post/createChat'
+import { getAllChats } from '../../api/get/getAllChats'
+import { setChatData } from '../../reducer/Slice'
+import { setLoading } from '../../reducer/UiSlice'
+
 
 
 
 const SingleContainer = ({ props }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
- 
-
+  const createChatFun = async () => {
+    dispatch(setLoading(true));
+    dispatch(setOpen(false));
+    const { data } = await createChat({ userId: props?._id });
+    const chats = await getAllChats();
+    dispatch(setChatData(chats?.data));
+    dispatch(setLoading(false));
+    navigate(`/chat/${data?._id}`);
+  }
   return (
-    <button style={{ width: "100%" }} onClick={() => { navigate(`/chat/${props?._id}`); dispatch(setOpen(false))}} key={`${props?._id}`}>
-      <div style={{ display: "flex", padding: "10px", alignItems: "center", gap: "8px", backgroundColor: "Gray", margin: "2px", borderRadius: "4px" }}>
+    <button style={{ width: "100%" }} onClick={()=>{createChatFun()}} key={`${props?._id}`}>
+      <div style={{ display: "flex", padding: "10px", alignItems: "center", gap: "8px", backgroundColor: "white", margin: "2px", borderRadius: "4px" }}>
         <Avatar src={props?.profilePic} alt='img' />
         <div>
-          <Typography variant="body1" style={{ color: "pink" }}>
+          <Typography variant="body1" style={{color:"#3498db"}} >
             {props?.name}
           </Typography>
-          <Typography variant="body2" style={{ color: "white" }}>
+          <Typography variant="body2" style={{ color: "" }}>
             {props?.latestMessage?.content}
           </Typography>
         </div>
@@ -30,7 +42,7 @@ const SingleContainer = ({ props }) => {
 function SearchContainer({ data }) {
   return (
     <div>
-      {data.map((item) => <SingleContainer key={item?._id} props={item} />)}
+      {data.map((item) => <SingleContainer key={item?._id} props={item} />)}  
     </div>
   )
 }
