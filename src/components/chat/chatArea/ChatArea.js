fe15fcea2773bece51fa7f0b1d578f8cb4351493
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-23 19:51:41 +0530

    update

--- src/components/chat/chatArea/ChatArea.js
+++ src/components/chat/chatArea/ChatArea.js
@@ -1,27 +1,50 @@
 import { Avatar, Typography, TextField, Hidden } from '@mui/material'
-import React, { useEffect, useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AiOutlineSend } from "react-icons/ai"
 import MessagesContainer from './MessagesContainer'
 import '../../styles.css'
 import { BiArrowBack } from "react-icons/bi"
-
-
+import { useSelector } from 'react-redux'
+import typingImg from "../../../assests/typingImg.gif"
+import { FaLink } from "react-icons/fa";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { useRef } from 'react'
+import ChatProfile from '../../navigation/ChatProfile'
 
 function ChatArea(props) {
+  const [open, setOpen] = useState(false);
+  const currentChatUser = useSelector((state) => state.userStore.currentChatUser);
+  const chatContainerRef = useRef(null);
+
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    }
+  }, [props.allMessages]);
+
+
+  const [isCopied, setIsCopied] = useState(false);
   var data = localStorage.getItem('loginInfo');
   data = JSON.parse(data);
   const navigate = useNavigate()
-  
+
   const [size, setSize] = useState({
     t: "16px",
     s: "12px"
   })
 
+  const handleCopyToClipboard = () => {
 
-  let value;  
- 
-  if (!props?.chatUserInfo?.isLoading) value = props?.chatUserInfo?.users[0]?._id === data?.id ? props?.chatUserInfo?.users[1] : props?.chatUserInfo?.users[0];
+    const linkText = window.location.href; 
+    navigator.clipboard.writeText(linkText)
+      .then(() => {
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 1500);
+      })
+      .catch((err) => console.error('Unable to copy to clipboard', err));
+  };
+
   return (
     <div style={{ backgroundColor: "white", }} className='chatArea'>
       <div style={{ display: "flex", gap: "12px", alignItems: "center", color: "grayText", padding: "4px" }} className='chatAreaOne'>
@@ -30,33 +53,50 @@ function ChatArea(props) {
             <BiArrowBack onClick={() => navigate("/")} />
           </div>
         </Hidden>
-        <div style={{ display: "flex", alignItems: "center", gap: "8px", cursor: "pointer" }}  >
-          <Avatar src={value?.profilePic} alt='name' />
-          <Typography variant="body1">{props?.chatUserInfo.isLoading ? "" : value?.name}</Typography>
+        <div style={{ display: "flex", alignItems: "center", gap: "8px", cursor: "pointer",overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap",maxWidth:"50%" }}  >
+          <Avatar src={currentChatUser?.profilePic} alt='name' />
+          <Typography variant="body1" >{currentChatUser?.name}</Typography>
         </div>
+        <div style={{ flexGrow: "1" }}></div>
+        <button onClick={() => handleCopyToClipboard()}>
+          <div style={{ backgroundColor: "white", padding: "8px", boxShadow: "0px px 2px 0px gray", border: "1px solid gray", borderRadius: "50%" }}>
+            <FaLink />
+          </div>
+        </button>
+        <button onClick={()=>setOpen(!open)}>
+          <div style={{ backgroundColor: "white", padding: "8px", boxShadow: "0px px 2px 0px gray", border: "1px solid gray", borderRadius: "50%" }}>
+            <BsThreeDotsVertical />
+            <ChatProfile open={open} setOpen={setOpen} current={currentChatUser} />
+          </div>
+        </button>
+        {isCopied && <div className="popup"> <FaLink /> Copied to clipboard!</div>}
       </div>
+
       <hr />
-      <div style={{ overflowY: "scroll" }} className='chatAreaTwo'>
-        {props?.chats.map((item, i) => {
-          return (
-            <MessagesContainer item={item} key={i} currentUser={data?.id} />
-          )
-        })}
-      </div>
+      <div style={{ overflowY: "scroll" }} className='chatAreaTwo' ref={chatContainerRef}>
+      {props?.allMessages?.map((item, i) => (
+        <MessagesContainer item={item} key={i} currentUser={data?.id} />
+      ))}
+      {props?.userIsTyping && (
+        <div style={{ display: "flex", justifyContent: `flex-start` }}>
+          <img src={typingImg} alt="" style={{ height: "40px" }} />
+        </div>
+      )}
+    </div>
 
       <div style={{ position: 'relative', display: "flex", gap: "8px", alignItems: "center", padding: "8px" }} className='chatAreaThree'>
         <TextField
           id="chatValue"
           label=""
           value={props?.message}
-          onChange={(e) => { props?.setMessage(e.target.value); if (e.target.value.length > 0) setSize({ t: "8px", s: "10px" }); else setSize({ t: "16px", s: "12px" }) }}
+          onChange={(e) => { props?.isTyping(); props?.setMessage(e.target.value); if (e.target.value.length > 0) setSize({ t: "8px", s: "10px" }); else setSize({ t: "16px", s: "12px" }) }}
           fullWidth
           variant="outlined"
           onClick={() => { setSize({ t: "8px", s: "10px" }) }}
           // on enter message send
           onKeyPress={(e) => {
             if (e.key === 'Enter')
-            props?.sendMessage();
+              props?.sendMessage();
           }}
           InputProps={{
             style: {
