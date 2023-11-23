6a70926bdcbbaa06248e1bcf94c8ec6fcc5aea44
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-23 19:52:00 +0530

    update

--- src/components/navigation/Profile.js
+++ src/components/navigation/Profile.js
@@ -11,6 +11,9 @@ import Avatar from '@mui/material/Avatar';
 // import commonData from '../../common-data/Common.json'
 import Typography from '@mui/material/Typography'
 import { MdOutlineEdit } from "react-icons/md";
+import { AiOutlineLogout } from "react-icons/ai";
+import { useNavigate } from 'react-router-dom';
+
 
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -20,8 +23,8 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 function Profile({ open, setOpen }) {
     var data = localStorage.getItem('loginInfo');
     data = JSON.parse(data);
-    console.log(data);
-    
+    const navigate = useNavigate()
+
     return (
         <>
             <Dialog
@@ -32,17 +35,26 @@ function Profile({ open, setOpen }) {
                 aria-describedby="alert-dialog-slide-description"
 
             >
-                <div style={{backgroundColor:"black",color:"white"}}>
-                    <DialogTitle style={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>{"Profile"}<MdOutlineEdit /></DialogTitle>
+                <div style={{ backgroundColor: "black", color: "white" }}>
+                    <DialogTitle style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>{"Profile"}<MdOutlineEdit /></DialogTitle>
                     <DialogContent>
-                        <div style={{ display:"flex" ,alignItems:"center",gap:"8px",color: "GrayText",marginBottom:"4px" }}>
+                        <div style={{ display: "flex", alignItems: "center", gap: "8px", color: "GrayText", marginBottom: "4px" }}>
                             <Avatar src={data?.profilePic} alt='user' />
                             <Typography variant="h6">{data?.name}</Typography>
                         </div>
-                        <DialogContentText id="alert-dialog-slide-description" style={{color:"GrayText"}}>
+                        <DialogContentText id="alert-dialog-slide-description" style={{ color: "GrayText" }}>
                             Email  : {data?.email}
                         </DialogContentText>
                     </DialogContent>
+                    <div style={{ display: "flex", justifyContent: "center", gap: "8px", padding: "8px", alignItems: "center" }}>
+                        <button style={{ display: "flex", alignItems: "center", gap: "8px" }} onClick={() => {
+                            navigate("/login");
+                            localStorage.removeItem('loginInfo');
+                        }}>
+                            <AiOutlineLogout style={{ cursor: "pointer" }} />
+                            <Typography variant="h6" style={{ cursor: "pointer" }}>Logout</Typography>
+                        </button>
+                    </div>
                 </div>
 
             </Dialog>
