8fe58316deadb766e0c97fcec424a7209eafea3a
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-23 19:51:25 +0530

    update

--- src/components/chat/allchat/AllChat.js
+++ src/components/chat/allchat/AllChat.js
@@ -1,29 +1,36 @@
-import React, {  useState } from 'react'
+import React, { useState } from 'react'
 import Navigation from '../../navigation/Navigation'
 // import Title from '../../common/Title'
 import MyChat from './MyChat'
 import SearchUser from '../../navigation/SearchUser'
 import Profile from '../../navigation/Profile'
+import { CircularProgress } from '@mui/material'
 
 
 
-
-function AllChat({chatData}) {
-  
+function AllChat({ chatData }) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
 
-
-
   return (
-    <div style={{  backgroundColor: "black", padding: "8px", border: "1px solid gray" }} className='AllChat'>
+    <div style={{ padding: "8px" }} className='AllChat'>
       {/* <Title title={`Let's chat`} size={'60px'} color={"white"} /> */}
       <Profile open={open} setOpen={setOpen} />
       <hr style={{ color: "white" }} />
       <Navigation handleOpen={handleOpen} />
-      <MyChat data={chatData} />
+      {chatData?.length > 0 ?
+        <>
+          <MyChat data={chatData} />
+        </> :
+        <>
+          <div style={{ display: "flex", flexDirection: "column", height: "80%", gap: "8px", alignItems: "center", justifyContent: "center" }}>
+            <CircularProgress />
+          </div>
+        </>
+      }
       <SearchUser />
     </div>
+
   )
 }
 
