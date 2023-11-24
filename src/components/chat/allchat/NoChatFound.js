9f43a3cc815567e37ea7092b21c52b274f6f9d3f
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-24 18:24:11 +0530

    update

---
+++ src/components/chat/allchat/NoChatFound.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+import { FaRocketchat } from "react-icons/fa";
+import { useDispatch } from 'react-redux';
+import { setOpen } from '../../../reducer/UiSlice';
+import { useSelector } from 'react-redux';
+import { CircularProgress } from '@mui/material'
+
+function NoChatFound() {
+  const dispatch = useDispatch()
+  const noChats = useSelector((state) => state.chatStore.NoChats);
+  return (
+    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "50vh" }} >
+      {noChats ?
+        <div style={{ backgroundColor: "white", height: "20vh", width: "60%", borderRadius: "4px", position: "relative" }} className='noChatFoundContainer'>
+          <div style={{ position: "absolute", top: "8px", left: "8px" }}>
+            <p >
+              <FaRocketchat />
+            </p>
+          </div>
+          <Typography variant="h6" style={{ fontWeight: "600" }}>
+            No Chat Found
+          </Typography>
+          <button className='makeAChat' onClick={() => dispatch(setOpen(true))}>
+            Create a chat
+          </button>
+        </div> :
+        <div style={{ display: "flex", flexDirection: "column", height: "80%", gap: "8px", alignItems: "center", justifyContent: "center" }}>
+          <CircularProgress />
+        </div>
+      }
+    </div>
+  )
+}
+
+export default NoChatFound
