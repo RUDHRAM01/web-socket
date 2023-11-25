fe926998bb9f73b7b0727c8e6a4fe9cddf0228d0
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-25 11:10:14 +0530

    update

--- src/components/chat/chatArea/MessagesContainer.js
+++ src/components/chat/chatArea/MessagesContainer.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { Decryption } from '../../Decryption'
 
 
 function MessagesContainer({ item, i, currentUser }) {
   let direction = currentUser === item?.sender?._id ? "flex-end" : "flex-start";
   let className = currentUser === item?.sender?._id ? "end" : "start";
+  const message = Decryption(item?.content,item?.iv)
   return (
     <>
       <div style={{ display: "flex", justifyContent: `${direction}` }}>
         <div className={className} style={{ padding: "8px", margin: "4px", maxWidth: "50%" }}>
-          <p>{item?.content}</p>
+          <p>{message}</p>
         </div>
       </div>
       
