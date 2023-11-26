c3158254b3aa18018dc5a87091ed3815cebb424f
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-11-26 19:12:17 +0530

    update

--- src/api/process.js
+++ src/api/process.js
@@ -1,29 +1,13 @@
 import axios from 'axios'
 
 const getRequest =  (path) => {
-    var data = localStorage.getItem('loginInfo');
-    data = JSON.parse(data);
-    
-    const config = {
-        headers: {
-            Authorization: `Bearer ${data?.token}`,
-        },
-    };
 
-    return axios.get(`${process.env.REACT_APP_MAIN}${path}`,config);
+    return axios.get(`${process.env.REACT_APP_MAIN}${path}`);
 }
 
 const postRequest =  (path, body) => {
-    var data = localStorage.getItem('loginInfo');
-    data = JSON.parse(data);
     
-    const config = {
-        headers: {
-            Authorization: `Bearer ${data?.token}`,
-        },
-    };
-
-    return axios.post(`${process.env.REACT_APP_MAIN}${path}`,body, config);
+    return axios.post(`${process.env.REACT_APP_MAIN}${path}`,body);
 }
 
 
