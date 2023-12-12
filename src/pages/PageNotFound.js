125468b9e6da42dba6fde8953bac222b1d012a8c
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-12 15:34:24 +0530

    404 update

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -5,12 +5,20 @@ import { useNavigate } from 'react-router-dom'
 
 function PageNotFound() {
     const navigate = useNavigate();
-
+    var data = localStorage.getItem('loginInfo');
+    data = JSON.parse(data);
+    const handlePageChange = () => {
+        if (data) {
+            navigate("/")
+        } else {
+            navigate("/login")
+       }
+    }
   return (
       <div style={{display:'flex',alignItems:"center",justifyContent:"center",height:"100vh",width:"100%",flexDirection:"column"}}>
           <img src={NotFound} alt="404" style={{ height: "200px", width: "400px", maxWidth: "90%" }} />
           <button
-                onClick={() => navigate("/")}
+                onClick={handlePageChange}
                 style={{
                     backgroundColor: "white",
                     border: "none",
