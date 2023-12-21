5290e85cca511192d34f4ab13b5f4e832c5ed3be
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-21 13:40:55 +0530

    process update

--- src/api/process.js
+++ src/api/process.js
@@ -1,24 +1,46 @@
 import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid'
+import CryptoJS from 'crypto-js'
+import cryptoRandomString from 'crypto-random-string'
+
 
 const getRequest =  (path) => {
-    var data = localStorage.getItem('loginInfo');
-    data = JSON.parse(data);
-
+    const requestId = uuidv4()
+	const token = cryptoRandomString({
+		length: 12,
+		type: 'base64',
+	})
     const config = {
+        withCredentials: true,
         headers: {
-            Authorization: `Bearer ${data?.token}`,
+            'x-app-type': 'RsRequest',
+            'x-request-id': requestId,
+            'x-hashed-id': CryptoJS.AES.encrypt(
+                requestId,
+                token
+            ).toString(),
+            'x-request-token': token,
         },
     };
     return axios.get(`${process.env.REACT_APP_MAIN}${path}`,config);
 }
 
-const postRequest =  (path, body) => {
-    var data = localStorage.getItem('loginInfo');
-    data = JSON.parse(data);
-
+const postRequest = (path, body) => {
+    const requestId = uuidv4()
+	const token = cryptoRandomString({
+		length: 12,
+		type: 'base64',
+	})
     const config = {
+        withCredentials: true,
         headers: {
-            Authorization: `Bearer ${data?.token}`,
+            'x-app-type': 'RsRequest',
+            'x-request-id': requestId,
+            'x-hashed-id': CryptoJS.AES.encrypt(
+                requestId,
+                token
+            ).toString(),
+            'x-request-token': token,
         },
     };
     return axios.post(`${process.env.REACT_APP_MAIN}${path}`,body,config);
