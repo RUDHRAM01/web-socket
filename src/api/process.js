a658c534aa44697ffe4624877f587bb31a483ae0
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-12-21 16:07:36 +0530

    u

--- src/api/process.js
+++ src/api/process.js
@@ -1,50 +1,52 @@
-import axios from 'axios'
-import { v4 as uuidv4 } from 'uuid'
-import CryptoJS from 'crypto-js'
-import cryptoRandomString from 'crypto-random-string'
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+import CryptoJS from 'crypto-js';
+import cryptoRandomString from 'crypto-random-string';
+import toast from 'react-hot-toast';
 
+const generateHeaders = () => {
+    const data = localStorage.getItem('loginInfo');
+    const to = JSON.parse(data)?.token;
+    const requestId = uuidv4();
+    const token = cryptoRandomString({
+        length: 12,
+        type: 'base64',
+    });
 
-const getRequest =  (path) => {
-    const requestId = uuidv4()
-	const token = cryptoRandomString({
-		length: 12,
-		type: 'base64',
-	})
+    return {
+        'Authorization': `Bearer ${to}`,
+        'x-app-type': 'RsRequest',
+        'x-request-id': requestId,
+        'x-hashed-id': CryptoJS.AES.encrypt(requestId, token).toString(),
+        'x-request-token': token,
+    };
+};
+
+const getRequest = (path) => {
     const config = {
         withCredentials: true,
-        headers: {
-            'x-app-type': 'RsRequest',
-            'x-request-id': requestId,
-            'x-hashed-id': CryptoJS.AES.encrypt(
-                requestId,
-                token
-            ).toString(),
-            'x-request-token': token,
-        },
+        headers: generateHeaders(),
     };
-    return axios.get(`${process.env.REACT_APP_MAIN}${path}`,config);
-}
+
+    return axios.get(`${process.env.REACT_APP_MAIN}${path}`, config)
+        .catch((error) => {
+            // Handle errors here
+            console.error('Error in GET request:', error);
+           toast.error(error?.response?.data?.msg || 'something went wrong' );
+        });
+};
 
 const postRequest = (path, body) => {
-    const requestId = uuidv4()
-	const token = cryptoRandomString({
-		length: 12,
-		type: 'base64',
-	})
     const config = {
         withCredentials: true,
-        headers: {
-            'x-app-type': 'RsRequest',
-            'x-request-id': requestId,
-            'x-hashed-id': CryptoJS.AES.encrypt(
-                requestId,
-                token
-            ).toString(),
-            'x-request-token': token,
-        },
+        headers: generateHeaders(),
     };
-    return axios.post(`${process.env.REACT_APP_MAIN}${path}`,body,config);
-}
 
+    return axios.post(`${process.env.REACT_APP_MAIN}${path}`, body, config)
+        .catch((error) => {
+            console.error('Error in POST request:', error);
+            toast.error(error?.response?.data?.msg || 'something went wrong' );
+        });
+};
 
-export {getRequest,postRequest}
+export { getRequest, postRequest };
