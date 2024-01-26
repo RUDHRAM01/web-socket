50b0883c42145d97f53932ca03250dcad0137686
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-01-26 19:57:54 +0530

    update

--- src/store.js
+++ src/store.js
@@ -3,13 +3,15 @@ import Slice from "./reducer/Slice"
 import UiSlice from "./reducer/UiSlice"
 import userSlice from "./reducer/userSlice"
 import statusSlice from "./reducer/StatusSlice"
+import NotificationSlice from "./reducer/NotificationSlice"
 
 const store = configureStore({
     reducer: {
         'chatStore': Slice,
         'uiStore': UiSlice,
         'userStore': userSlice,
-        'statusStore': statusSlice
+        'statusStore': statusSlice,
+        'notificationStore': NotificationSlice
     }
 })
 
