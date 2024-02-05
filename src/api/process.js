7a541f02dd933faa60623a5bc3238f0fa6ab389b
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-02-05 15:17:18 +0530

    up

--- src/api/process.js
+++ src/api/process.js
@@ -4,6 +4,10 @@ import CryptoJS from 'crypto-js';
 import cryptoRandomString from 'crypto-random-string';
 
 const generateHeaders = () => {
+    const data = localStorage.getItem('loginInfo');
+    let to = JSON.parse(data);
+    to = to?.token;
+
     const requestId = uuidv4();
     const token = cryptoRandomString({
         length: 12,
@@ -11,6 +15,7 @@ const generateHeaders = () => {
     });
 
     return {
+        'Authorization': `Bearer ${to}`,
         'x-app-type': 'RsRequest',
         'x-request-id': requestId,
         'x-hashed-id': CryptoJS.AES.encrypt(requestId, token).toString(),
