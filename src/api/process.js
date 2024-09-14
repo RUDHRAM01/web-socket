78216a015cee3805a571493accef8d4d612216bb
RUDHRAM SARASWAT <90370450+RUDHRAM01@users.noreply.github.com>
2024-09-14 17:49:44 +0530

    Update process.js

--- src/api/process.js
+++ src/api/process.js
@@ -1,81 +1,44 @@
-import axios from "axios";
-import { v4 as uuidv4 } from "uuid";
-import CryptoJS from "crypto-js";
-import cryptoRandomString from "crypto-random-string";
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+import CryptoJS from 'crypto-js';
+import cryptoRandomString from 'crypto-random-string';
 
 const generateHeaders = () => {
-  const data = localStorage.getItem("loginInfo");
-  let to = JSON.parse(data);
-  to = to?.token;
-
-  const requestId = uuidv4();
-  const token = cryptoRandomString({
-    length: 12,
-    type: "base64",
-  });
-
-  return {
-    Authorization: `Bearer ${to}`,
-    "x-app-type": "RsRequest",
-    "x-request-id": requestId,
-    "x-hashed-id": CryptoJS.AES.encrypt(requestId, token).toString(),
-    "x-request-token": token,
-  };
-};
-
-const isTokenExpired = (error) => {
-  if (
-    error.response.status === 401 &&
-    error.response.data.msg === "token-exp"
-  ) {
-    const token = error.response.data.token;
-    const data = localStorage.getItem("loginInfo");
+    const data = localStorage.getItem('loginInfo');
     let to = JSON.parse(data);
-    to.token = token;
-    localStorage.setItem("loginInfo", JSON.stringify(to));
-    return true;
-  }
-  return false;
+    to = to?.token;
+
+    const requestId = uuidv4();
+    const token = cryptoRandomString({
+        length: 12,
+        type: 'base64',
+    });
+
+    return {
+        'Authorization': `Bearer ${to}`,
+        'x-app-type': 'RsRequest',
+        'x-request-id': requestId,
+        'x-hashed-id': CryptoJS.AES.encrypt(requestId, token).toString(),
+        'x-request-token': token,
+    };
 };
 
-const getRequest = async (path) => {
-  const config = {
-    withCredentials: true,
-    headers: generateHeaders(),
-  };
+const getRequest = (path) => {
+    const config = {
+        withCredentials: true,
+        headers: generateHeaders(),
+    };
 
-  try {
-    const data = await axios.get(
-      `${process.env.REACT_APP_MAIN}${path}`,
-      config
-    );
-    return data;
-  } catch (error) {
-    if (isTokenExpired(error)) {
-      return getRequest(path);
-    }
-    return error;
-  }
+    return axios.get(`${process.env.REACT_APP_MAIN}${path}`, config)
 };
 
-const postRequest = async (path, body) => {
-  const config = {
-    withCredentials: true,
-    headers: generateHeaders(),
-  };
-  try {
-    const data = await axios.post(
-      `${process.env.REACT_APP_MAIN}${path}`,
-      body,
-      config
-    );
-    return data;
-  } catch (error) {
-    if (isTokenExpired(error)) {
-      return postRequest(path, body);
-    }
-    return error;
-  }
+const postRequest = (path, body) => {
+    const config = {
+        withCredentials: true,
+        headers: generateHeaders(),
+    };
+
+    return axios.post(`${process.env.REACT_APP_MAIN}${path}`, body, config)
 };
 
 export { getRequest, postRequest };
